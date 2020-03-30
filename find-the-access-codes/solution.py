240cd7462ebda8c8b3aeb57f352f9a75828996b4
Mateo Upegui Borja <upeguiborja@gmail.com>
2020-03-30 04:42:01 -0500

    Solution #4 v2: Too slow

--- find-the-access-codes/solution.py
+++ find-the-access-codes/solution.py
@@ -1,14 +1,27 @@
 def solution(l):
-    ## Could specify the appropiate permutation/combination if the 
-    # list has just one item repeted multiple times
-    # A test fails because of poorly optimized code
-    l = l[::-1]
+    # Graph based solution ROCKS!!!
     result = 0
-    while len(l) > 2:
-        i = l.pop()
-        l_mod = [item for item in l if item % i == 0]
-        while len(l_mod) > 1:
-            j = l_mod.pop()
-            l_mod_2 = [item for item in l_mod if item % j == 0]
-            result += len(l_mod_2)
+    
+    # Calculating adjacency matrix for the list where each index is a 
+    # vertex and vertices are connected if the are divisible, and the 
+    # same vertex is not connected 
+    M = [] # Adjacency Matrix
+    _len = len(l)
+    for i in range(_len):
+        _buff = []
+        for j in range(_len):
+            _c = 1 if l[j] % l[i] == 0 and i < j else 0
+            _buff.append(_c)
+        M.append(_buff)
+        M[i][i] = 0
+    
+    # Naive matrix multiplication to obtain M ^ 3, could use Strassen
+    # Algorithm for even faster resolution or maybe use the special
+    # propery of this matrix which is that the diagonal is 0 to reduce it,
+    # M is an upper triangular matrix as M^2 and M^3
+    for j in range(_len):
+        for i in range(j):
+            for k in range(i,j):
+                result += M[i][k]*M[k][j]
+
     return result
